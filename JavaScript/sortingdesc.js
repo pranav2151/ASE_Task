1c7a30a599838b6b1756e3c0712859c659efd44f
Pranav Shelke <86565193+pranav2151@users.noreply.github.com>
2023-10-21 17:32:02 +0530

    Add files via upload

---
+++ JavaScript/sortingdesc.js
@@ -0,0 +1,16 @@
+function sortArrayDescending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const inputArray = [4, 2, 9, 5, 1];
+const sortedArray = sortArrayDescending(inputArray);
+console.log(sortedArray);
